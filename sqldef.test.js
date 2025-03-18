01c9f241f05eaa502c7caf8f2f1d1888116668a4
David Konsumer <konsumer@jetboystudio.com>
2025-03-18 04:48:06 -0700

    tests for every type

--- sqldef.test.js
+++ sqldef.test.js
@@ -17,7 +17,58 @@ test('should be able to diff some mysql', async ({ assert }) => {
   ) Engine=InnoDB DEFAULT CHARSET=utf8mb4;
   `
   const r = await sqldef('mysql', sql1, sql2)
-  assert.equal(r, 'ALTER TABLE `user` DROP COLUMN `created_at`')
+  assert.snapshot(r)
+})
+
+test('should be able to diff some mssql', async ({ assert }) => {
+  const sql1 = `
+  CREATE TABLE dbo.Employee (
+    EmployeeID INT PRIMARY KEY
+  );
+  `
+
+  const sql2 = `
+  CREATE TABLE dbo.Employee (
+    Employee INT PRIMARY KEY
+  );
+  `
+  const r = await sqldef('mssql', sql1, sql2)
+  assert.snapshot(r)
+})
+
+test('should be able to diff some sqlite', async ({ assert }) => {
+ const sql1 = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY
+  );
+  `
+
+  const sql2 = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY,
+    name VARCHAR NOT NULL
+  );
+  `
+  const r = await sqldef('sqlite', sql1, sql2)
+  assert.snapshot(r)
+})
+
+test('should be able to diff some postgres', async ({ assert }) => {
+ const sql1 = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY
+  );
+  `
+
+  const sql2 = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    location point
+  );
+  `
+  const r = await sqldef('postgres', sql1, sql2)
+  assert.snapshot(r)
 })
 
 test('should throw on bad SQL', async ({assert}) => {
@@ -25,6 +76,6 @@ test('should throw on bad SQL', async ({assert}) => {
     await sqldef('mysql', 'BAD STUFF', 'NOT SQL, SORRRY')
     assert.equal(true, 'Should have thrown')
   } catch(e) {
-    assert.equal(true, e.message.includes('found syntax error when parsing DDL'), `Error is not correct: ${e.message}`)
+    assert.snapshot(e.message)
   }
 })
