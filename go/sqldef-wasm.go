0fb7401215d78f921f03e87d6cd4e6bfcd637a7d
David Konsumer <konsumer@jetboystudio.com>
2025-03-18 05:56:25 -0700

    tidy up

--- go/sqldef-wasm.go
+++ go/sqldef-wasm.go
@@ -1,89 +1,68 @@
 package main
 
 import (
-  "strings"
-  "syscall/js"
+	"fmt"
+	"strings"
+	"syscall/js"
 
-  "github.com/sqldef/sqldef/database"
-  "github.com/sqldef/sqldef/parser"
-  "github.com/sqldef/sqldef/schema"
+	"github.com/sqldef/sqldef/database"
+	"github.com/sqldef/sqldef/parser"
+	"github.com/sqldef/sqldef/schema"
 )
 
 // diff takes SQL DDL statements and generates the difference
 func diff(mode string, desiredDDLs string, currentDDLs string) (string, error) {
-  generatorMode := schema.GeneratorModeMysql
-  sqlParser := database.GenericParser{}
-  
-  if mode == "postgres" {
-    generatorMode = schema.GeneratorModePostgres
-    sqlParser = database.NewParser(parser.ParserModePostgres)
-  }
+	var generatorMode schema.GeneratorMode
+	var sqlParser database.Parser
 
-  if mode == "sqlite3" {
-    generatorMode = schema.GeneratorModeSQLite3
-    sqlParser = database.NewParser(parser.ParserModeSQLite3)
-  }
+	switch mode {
+	case "postgres":
+		generatorMode = schema.GeneratorModePostgres
+		sqlParser = database.NewParser(parser.ParserModePostgres)
+	case "sqlite3":
+		generatorMode = schema.GeneratorModeSQLite3
+		sqlParser = database.NewParser(parser.ParserModeSQLite3)
+	case "mssql":
+		generatorMode = schema.GeneratorModeMssql
+		sqlParser = database.NewParser(parser.ParserModeMssql)
+	case "mysql":
+		generatorMode = schema.GeneratorModeMysql
+		sqlParser = database.GenericParser{}
+	default:
+		return "", fmt.Errorf("Invalid type: %s. Use mysql/sqlite3/mssql/postgres", mode)
+	}
 
-  if mode == "mssql" {
-    generatorMode = schema.GeneratorModeMssql
-    sqlParser = database.NewParser(parser.ParserModeMssql)
-  }
+	ddls, err := schema.GenerateIdempotentDDLs(generatorMode, sqlParser, desiredDDLs, currentDDLs, database.GeneratorConfig{}, "")
+	if err != nil {
+		return "", err
+	}
 
-  ddls, err := schema.GenerateIdempotentDDLs(generatorMode, sqlParser, desiredDDLs, currentDDLs, database.GeneratorConfig{}, "")
-  out := strings.Join(ddls, ";\n")
-  
-  if err != nil {
-    return "", err
-  } else {
-    return out, nil
-  }
+	return strings.Join(ddls, ";\n"), nil
 }
 
-// diffWrapper creates a JS-compatible function for the diff operation
-func diffWrapper() js.Func {
-  return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-    if len(args) != 3 {
-      return map[string]interface{}{
-        "error": "Invalid number of arguments. Expected 3: mode, desiredDDLs, currentDDLs",
-      }
-    }
-
-    mode := args[0].String()
-    desiredDDLs := args[1].String()
-    currentDDLs := args[2].String()
-
-    result, err := diff(mode, desiredDDLs, currentDDLs)
-    if err != nil {
-      return map[string]interface{}{
-        "error": err.Error(),
-      }
-    }
-
-    return map[string]interface{}{
-      "result": result,
-    }
-  })
-}
-
-// createSqlDefModule creates an object with all exported functions
-func createSqlDefModule() map[string]interface{} {
-  return map[string]interface{}{
-    "diff": diffWrapper(),
-  }
-}
-
-// registerModuleFactory registers a function that returns the module
-func registerModuleFactory() js.Func {
-  moduleFactory := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-    return createSqlDefModule()
-  })
-  
-  js.Global().Set("createSqlDefModule", moduleFactory)
-  return moduleFactory
+func jsDiff(this js.Value, args []js.Value) interface{} {
+	if len(args) != 3 {
+		return map[string]interface{}{
+			"error": "Invalid number of arguments. Expected 3: mode, desiredDDLs, currentDDLs",
+		}
+	}
+	result, err := diff(args[0].String(), args[1].String(), args[2].String())
+	if err != nil {
+		return map[string]interface{}{
+			"error": err.Error(),
+		}
+	}
+	return map[string]interface{}{
+		"result": result,
+	}
 }
 
 func main() {
-  moduleFactory := registerModuleFactory()
-  <-make(chan bool)
-  moduleFactory.Release()
+	module := map[string]interface{}{
+		"diff": js.FuncOf(jsDiff),
+	}
+	js.Global().Set("createSqlDefModule", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		return module
+	}))
+	<-make(chan bool)
 }
